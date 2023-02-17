6e749fcc014ac920b97b4c9a2d3731297c84c002
Rabindranath Ferreira <ferreiralox@gmail.com>
2023-02-17 17:25:24 +0100

    using position on array web elements first, last and any index with eq, also parent and child web element locator

--- cypress/integration/4-Site-test/ui-help.spec.js
+++ cypress/integration/4-Site-test/ui-help.spec.js
@@ -16,4 +16,18 @@ describe('Should test UI', () => {
         // cy.get('ul > :nth-child(2)').shadow().should('have.text', 'In a list!')
         cy.get('ul > :nth-child(2)').should('have.text', 'In a list!')
     })
+
+    it('should get first and alst element', () => {
+        cy.contains('Dynamic Content').click()
+        cy.get('img').first().should('be.visible') // toma el 1ro
+        cy.get('img').eq(2).should('be.visible') // evalua la posicion entre 0 y n-1
+        cy.get('img').last().should('be.visible') // toma el 2do
+    })
+
+    it('should use parent and children', () => {
+        cy.contains('Dynamic Content').click()
+        cy.get(':nth-child(4) > .large-2 > img').parent() // retorma el elemento padre de este elemento en el DOM
+        cy.get('.example > :nth-child(7)').children() // retorma el elemento hijo de este elemento en el DOM
+    })
+
 })
